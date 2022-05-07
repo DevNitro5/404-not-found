45f081eeb77c2587e279798af751ab58dae5bb64
Anish Chhetri <anishchhetri196@gmail.com>
2022-05-07 12:21:18 +0545

    Project 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        first: "'Inconsolata'",
+        second: "'Space Mono'",
+      },
+    },
+  },
+  plugins: [],
+};
